e98cd290b818c1c7f72195ed19888ffa8d416dde
lochungtin <lochungtin@gmail.com>
2023-03-20 18:34:16 +0000

    remove shared assets

--- nextjs/src/pages/index.js
+++ nextjs/src/pages/index.js
@@ -1,14 +1,14 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
 	return (
 		<>
 			<Head>
 				<title>FullFrame NextJS</title>
-				<meta name="description" content="Project FullFrame App" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
+				<meta name='description' content='Project FullFrame App' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='../../assets/favicon.ico' />
 			</Head>
 		</>
-	)
+	);
 }
